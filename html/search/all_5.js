70b00b1c5df4102593f2d70607623889fdeb50ab
Hpsaturn <hpsaturn@gmail.com>
2022-01-28 12:40:47 +0100

    v0.5.1 of CanAirIO Sensorlib documentation

--- html/search/all_5.js
+++ html/search/all_5.js
@@ -1,5 +1,7 @@
 var searchData=
 [
-  ['readallsensors',['readAllSensors',['../classSensors.html#a4cd0c5e7d7c7230b32e8313e5f652a9d',1,'Sensors']]],
-  ['resetnextunit',['resetNextUnit',['../classSensors.html#ae6ac6080b6ad79ba9cd441646826058d',1,'Sensors']]]
+  ['readallsensors',['readAllSensors',['../classSensors.html#a4f531daf0233f48c3c4e5e91688f90ea',1,'Sensors']]],
+  ['resetnextunit',['resetNextUnit',['../classSensors.html#ae6ac6080b6ad79ba9cd441646826058d',1,'Sensors']]],
+  ['resetsensorsregister',['resetSensorsRegister',['../classSensors.html#a86190ba0e2fce178aaf936d2c926f7fe',1,'Sensors']]],
+  ['resetunitsregister',['resetUnitsRegister',['../classSensors.html#a47635ad00b96b1175c96c6ead2c4a514',1,'Sensors']]]
 ];
